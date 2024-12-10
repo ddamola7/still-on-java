cf2679d7d42b6a751aa04448cf224be5649a7b9c
oyindamola <ddamola7@gmail.com>
2024-12-10 12:35:31 +0100

    intro to java

---
+++ index.js
@@ -0,0 +1,100 @@
+
+// variable initialization
+var fullName =  "adediran oyindamola"
+var married = "false"
+var numberofChildren = 2
+var disability = null
+var accountNumber = undefined
+
+// console.log
+console.log("fullName");
+console.log('numberofChildren');
+console.log('20+20');
+console.log("hello world");
+
+// string operators
+var sentence = 'i am going to be a very skilled software developer'
+console.log(sentence.length);
+console.log(sentence.toUpperCase());
+console.log(sentence.toLowerCase());
+
+// concatenation
+var favoriteTeam = 'chelsea'
+var favoritePlayer = 'felix'
+var ability = 'excellent'
+var sentence2 = 'my favorite Player is ' + favoritePlayer + 'he plays for' + favoriteTeam + 'and he is' + ability
+console.log(sentence2);
+
+//  arithmetic operators
+var x = 5
+var y = 3
+var z = 7
+console.log(z-x);
+console.log(x+y);
+console.log(z*y);
+console.log(z%x);
+console.log(x**y);
+console.log(z/y); 
+
+// math operators
+console.log(Math.sqrt(144));
+console.log(Math.pow(16,8));
+console.log(Math.round(4.6));
+console.log(Math.round(4.4));
+console.log(Math.floor(4.6));
+console.log(Math.ceil(4.4));
+
+// assignment operators
+var num1 = 25
+var num2 = 35
+var num3 = 15
+
+num1 += num3 
+console.log(num1);
+
+num2 -= num3
+num3 -= num2
+console.log(num3);
+
+//  comparison operators
+console.log(5==5);
+console.log(5==6);
+console.log(5=='5');
+
+console.log(7==='7'); 
+
+console.log(5!=6);
+console.log(5!='5');
+console.log(100>17);
+console.log(18>=18); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
